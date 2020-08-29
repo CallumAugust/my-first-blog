21ce8e7052574b0e3dcb7dce492541cf86f0594c
Callum August <callum@august-family.org.uk>
2020-08-29 12:44:44 +0100

    theme changes

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -5,7 +5,7 @@ from .forms import PostForm
 
 
 def post_list(request):
-	posts = Post.objects.all().order_by('published_date')
+	posts = Post.objects.all().order_by('-published_date')
 	return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
