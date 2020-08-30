62cc03098eee21a535430bbe659fc478ec9c30f0
Callum August <callum@august-family.org.uk>
2020-08-30 12:56:04 +0100

    Test find CV button & click on it

--- functionality_test.py
+++ functionality_test.py
@@ -1,11 +1,31 @@
 from selenium import webdriver
+import unittest
 
-#An employer wishes to view the CV
-#On the blog homepage they see a link in the header which says CV
-#They click on the link and are re-directed to the CV page
+class EmployeeVisitor(unittest.TestCase):
+
+	def setUp(self):
+		self.browser = webdriver.Firefox()
+	
+	def tearDown(self):
+		self.browser.quit()
+
+	def test_can_visit_cv_page(self):
+		self.browser.get('http://127.0.0.1:8000')
+		#An employer wishes to view the CV
+		#On the blog homepage they see a link in the header which says CV
+		header_text = header_text = self.browser.find_element_by_tag_name('h2').text
+		self.assertIn('CV', header_text)
+		#They click on the link and are re-directed to the CV page
+		link = self.browser.find_element_by_link_text('CV')
+		#The page title is CV
+		self.fail("Finish this Test!")
+
+
 #At the top of the page is Callum August as a title and Curriculum vitae as a subtitle
 #Below that there is an address, email and phone number.
-#They see 5 collapsable sections titled: "Personal Statement", "Education", "Tech Skills", "Work Experience" and "Interests"
-#They click on Education and see the list of qualifications
-#Then they click on Education again and the list collapses
+#They see 5 sections titled: "Personal Statement", "Education", "Tech Skills", "Work Experience" and "Interests"
+#Within each section they see the information
 #The employer is done looking at the CV and clicks a link "Blog" in order to go back to the blog.
+
+if __name__ == '__main__':
+	unittest.main(warnings='ignore')
