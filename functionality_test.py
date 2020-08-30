927234a6911bb770024e1e4f04a94cb19ac17558
Callum August <callum@august-family.org.uk>
2020-08-30 12:12:37 +0100

    Added functionality test, wrote story from employers perspective

---
+++ functionality_test.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+
+#An employer wishes to view the CV
+#On the blog homepage they see a link in the header which says CV
+#They click on the link and are re-directed to the CV page
+#At the top of the page is Callum August as a title and Curriculum vitae as a subtitle
+#Below that there is an address, email and phone number.
+#They see 5 collapsable sections titled: "Personal Statement", "Education", "Tech Skills", "Work Experience" and "Interests"
+#They click on Education and see the list of qualifications
+#Then they click on Education again and the list collapses
+#The employer is done looking at the CV and clicks a link "Blog" in order to go back to the blog.
